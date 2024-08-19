249f8585c630b8b2d2c301d05d41a0d693c2d934
Merlin Beutlberger <m.beutlberger@sap.com>
2024-08-19 16:49:36 +0200

    Add error to file containing warning

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,5 +1,6 @@
-sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport"], (UIComponent) => {
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport", "sap/m/DateTimeInput"], (UIComponent, DateTimeInput) => {
 	"use strict";
+	const dti = new DateTimeInput();
 	return UIComponent.extend("sap.ui.demo.todo.Component", {
 		
 	});
